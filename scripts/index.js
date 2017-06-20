9d43e5be4d847c6aa2545f354558ecc3c8fa6a1b
Andrushkins <drachyov.andrew@gmail.com>
2017-06-20 23:12:55 +0300

    js for mobile

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
   var popup = document.querySelector('.about_popup'),
       video = document.querySelector('.video'),
       about = document.querySelector('#about_button'),
-      close = document.querySelector('.close_btn');
+      close = document.querySelector('.close_btn'),
+       logo = document.querySelector('.logo');
 
   close.addEventListener('click', function () {
     popup.style.display = 'none';
@@ -13,4 +14,11 @@ document.addEventListener('DOMContentLoaded', function () {
     popup.style.display = 'block';
     video.style.display = 'none';
   });
+
+  if (navigator.platform == 'iPad' || navigator.platform == 'iPhone' || navigator.platform == 'iPod' || navigator.platform == 'Linux armv6l') {
+    logo.style.top = (window.innerHeight / 2) - (logo.clientHeight / 6) + 'px';
+    window.onscroll = function() {
+      logo.style.top = (window.innerHeight / 2) - (logo.clientHeight / 6) + 'px';
+    }
+  }
 });
