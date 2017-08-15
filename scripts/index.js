2f8fd6c2a13e82e2347aa94a520a827386ee90e3
Andrushkins <drachyov.andrew@gmail.com>
2017-08-15 10:40:50 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -8,25 +8,15 @@ document.addEventListener('DOMContentLoaded', function () {
   close.addEventListener('click', function () {
     popup.style.display = 'none';
     video.style.display = 'block';
-    video.children[0].play();
   });
 
   about.addEventListener('click', function () {
     popup.style.display = 'block';
     video.style.display = 'none';
-    video.children[0].pause();
   });
 
   window.addEventListener("orientationchange", function() {
     logo.style.top = (window.innerHeight / 2) - (logo.clientHeight / 6) + 'px';
-  });
-
-  video.children[0].addEventListener('click', function(e) {
-    if (e.target.paused) {
-      e.target.play();
-    } else {
-      e.target.pause();
-    }
   });
 
   if (navigator.platform == 'iPad' || navigator.platform == 'iPhone' || navigator.platform == 'iPod' || navigator.platform == 'Linux armv6l') {
